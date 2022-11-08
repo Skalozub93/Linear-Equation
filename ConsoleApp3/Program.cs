d5a54df0d606282ae51cc4e7fee04805b0ad9f2c
Vladislav Skalozub <vladskalozub97@gmail.com>
2022-11-08 20:04:55 +0200

    Equation has been rewrite

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -20,49 +20,45 @@ namespace ConsoleApp3
             Console.WriteLine("Введите число c: ");
             var c = int.Parse(Console.ReadLine());
              
+
+            if(a == 0 && b == 0 && c == 0)
+            {
+                Console.WriteLine("-1");
+            }
+            else if(a == 0 && b == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if(a == 0)
+            {
+                Console.WriteLine("1" + -c / b);
+            }
             var DisResult = (Math.Pow(b, 2) - 4 * a * c);
 
-            if (DisResult < 0)
-            {
-                throw new Exception("Некоректная запись!");
-            }
-            else if(a == 0 || b == 0 || c == 0)
-            {
-                throw new Exception("Некоректная запись!");
-            }
-            
-            
-
-            Console.WriteLine("Дискриминант = " + DisResult);
-
-            double x1;
-            double x2;
+            double x0 = 0;
+            double x1 = 0;
+            double x2 = 0;
 
             var TwoMultiplyA = 2 * a;
 
-            if(a == 0)
-            {
-                throw new Exception("На ноль делить нельзя!!!");
-            }
+            Console.WriteLine("Дискриминант = " + DisResult);
 
-            if (DisResult == 0)
+            if (DisResult < 0)
             {
-                x1 = -b / (2 * a);
-                x2 = x1;
+                throw new Exception("Нет корней!");
             }
-            else
+            if(DisResult == 0)
             {
-                
+                Console.WriteLine(x0 = -b / (2 * a));
+                Console.WriteLine($"x0 = {x0}");
+            }
+            else if (DisResult > 0)
+
                 x1 = (-b + Math.Sqrt(DisResult)) / TwoMultiplyA;
                 x2 = (-b - Math.Sqrt(DisResult)) / TwoMultiplyA;
+                Console.WriteLine($"x1 = {x1}");
+                Console.WriteLine($"x2 = {x2}");
             }
-
-            Console.WriteLine($"x1 = {x1}");
-            Console.WriteLine($"x2 = {x2}");
-
-        }  
-
-        
-
+               
     }
 }
