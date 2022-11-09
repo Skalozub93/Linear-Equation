2296156ee576f9b8e62cc024850ff5fe54ecadab
Vladislav Skalozub <vladskalozub97@gmail.com>
2022-11-09 21:03:06 +0200

    uptade

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -5,31 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace ConsoleApp3
+namespace LinerEquation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Напишите квадратное уравнение:");
+          
+            int a = NameParameter("a");
+            int b = NameParameter("b");
+            int c = NameParameter("c");
 
-            Console.WriteLine("Введите число a: ");
-            var a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите число b: ");
-            var b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Введите число c: ");
-            var c = int.Parse(Console.ReadLine());
-             
-
-            if(a == 0 && b == 0 && c == 0)
+            if (a == 0 && b == 0 && c == 0)
             {
                 Console.WriteLine("-1");
             }
-            else if(a == 0 && b == 0)
+            else if (a == 0 && b == 0)
             {
                 Console.WriteLine("0");
             }
-            else if(a == 0)
+            else if (a == 0)
             {
                 Console.WriteLine("1" + -c / b);
             }
@@ -41,24 +37,44 @@ namespace ConsoleApp3
 
             var TwoMultiplyA = 2 * a;
 
-            Console.WriteLine("Дискриминант = " + DisResult);
+            if(TwoMultiplyA == 0)
+            {
+                throw new Exception("Делить на ноль нельзя!");
+            }
 
+            Console.WriteLine("Дискриминант = " + DisResult);
+            
+            
             if (DisResult < 0)
             {
                 throw new Exception("Нет корней!");
             }
-            if(DisResult == 0)
+            if (DisResult == 0)
             {
                 Console.WriteLine(x0 = -b / (2 * a));
                 Console.WriteLine($"x0 = {x0}");
             }
             else if (DisResult > 0)
-
+            {
                 x1 = (-b + Math.Sqrt(DisResult)) / TwoMultiplyA;
                 x2 = (-b - Math.Sqrt(DisResult)) / TwoMultiplyA;
                 Console.WriteLine($"x1 = {x1}");
                 Console.WriteLine($"x2 = {x2}");
             }
-               
+        }
+
+        static public int NameParameter(string name)
+        {
+            Console.Write($"Введите значение {name}\t");
+
+            if (!int.TryParse(Console.ReadLine(), out var variable))       
+            {
+                throw new Exception($"Значение {name} некоректное" + "\tнапишите цифру!");
+                
+            }
+
+            return variable;
+        }
     }
+   
 }
